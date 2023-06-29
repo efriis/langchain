570ca96593463c75f66110c52978699eb46f54ca
Erick Friis <erickfriis@gmail.com>
2023-06-29 13:06:08 -0700

    use api, and upload function

--- langchain/hub/load.py
+++ langchain/hub/load.py
@@ -1,28 +1,57 @@
+import urllib.error
 import urllib.request
-from pathlib import Path
 from typing import Any, Dict, Optional
 
+from langchain.hub._api import HubApi
+from langchain.load.dump import dumps
 from langchain.load.load import loads
 from langchain.load.serializable import Serializable
-from langchain.utils import get_from_dict_or_env
 
-HUB_KEY = "hub_base_url"
-HUB_ENV_KEY = "HUB_BASE_URL"
-HUB_DEFAULT = "http://localhost:4000/"
+OBJECT_PATH = "main.lc.json"
 
-HUB_API_KEY_KEY = "chainhub_api_key"
-HUB_API_KEY_ENV_KEY = "CHAINHUB_API_KEY"
+def download(owner: str, repo: str, ref: Optional[str] = None, *, secrets_map: Optional[Dict[str, str]] = None, **kwargs) -> Any:
+    # can raise filenotfound error
+    api = HubApi(**kwargs)
+    resp = api.get_repo_file(owner, repo, OBJECT_PATH, ref)
+    content = resp["content"].decode()
+    
+    return loads(content, secrets_map=secrets_map)
 
-MAIN_BRANCH = "main"
-OBJECT_PATH = "object.lc.json"
+def upload(object: Serializable, owner: str, repo: str, upsert: bool = False, *, autoconfirm: bool = False, **kwargs) -> None:
+    # ONLY SUPPORT UPLOAD TO USER - NO ORGS OR SHARED YET
+    """
+    if repo doesn't exist:
+        create repo
 
+    if file exists:
+        get sha
+        update file
+    else:
+        create file with contents
+    """
+    # check if repo exists, get file sha if so
+    api = HubApi(**kwargs)
 
+    # check if repo exists
+    try:
+        api.get_repo(owner, repo)
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            # repo doesn't exist, create it
+            api.create_repo(owner, repo)
+        else:
+            raise e
+    
+    # get existing sha if file exists
+    existing_file = api.get_repo_file(owner, repo, OBJECT_PATH)
 
-def download(path: str, ref: str = MAIN_BRANCH, *, secrets_map: Optional[Dict[str, str]] = None, **kwargs) -> Any:
-    hub_base_url = get_from_dict_or_env(kwargs, HUB_KEY, HUB_ENV_KEY, HUB_DEFAULT)
-    url = f"{hub_base_url}{path}/raw/branch/{ref}/{OBJECT_PATH}"
+    # content
+    content = dumps(object).encode()
 
-    # TODO(erick): handle errors
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode()
-        return loads(content, secrets_map=secrets_map)
+    # if not, create it with file contents
+    if existing_file is None:
+        api.create_repo_file(owner, repo, OBJECT_PATH, content=content)
+    else:
+        existing_sha = existing_file["sha"]
+        # else update the file
+        api.update_repo_file(owner, repo, OBJECT_PATH, content=content, sha=existing_sha)
